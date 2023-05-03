258c5504b179af04532f09cdbac9160e6bbac0bd
Chandan-Chandra-shil <chandanchandrashil22@gmail.com>
2023-05-03 19:57:38 +0600

    view Recipes data added

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
-const chefDetail = require('./data/chefDetail.json')
+const viewRecipes = require('./data/viewRecipes.json')
+const cors = require('cors')
 
+app.use(cors())
 app.get('/', (req, res) => {
   res.send('recipe guru is running .....')
 })
-app.get('/chefDetail', (req, res) => {
-  res.send(chefDetail)
+app.get('/viewRecipes', (req, res) => {
+  res.send(viewRecipes)
+})
+
+app.get('/viewRecipes/:id', (req, res) => {
+  const id = req.params.id;
+  
+  const details = viewRecipes.find(n => n.id == id);
+  res.send(details)
 })
 
 app.listen(port, () => {
